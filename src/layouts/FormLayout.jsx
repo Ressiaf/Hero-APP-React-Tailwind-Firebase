8e83440c0fcd77485add8983a465a2a527d8b9b5
Ressiaf <facurd91@gmail.com>
2022-09-01 21:09:42 -0300

    background update

--- src/layouts/FormLayout.jsx
+++ src/layouts/FormLayout.jsx
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Venombackground from "../../dist/assets/images/Venombackground"
 
 const FormLayout = ( ) => {
   return (
     <>
-      <div style={{backgroundImage: Venombackground }} className="bg-cover h-screen w-full" >
+      <div className="bg-[url('../../dist/assets/images/Venombackground')] bg-cover h-screen w-full   ">
       <div className="bg-black/30 py-1 h h-screen">
         <div className="w-3/4  h-1/4 lg:w-1/3  mx-auto mt-0 flex flex-col items-stretch  font-thin ">
           <Outlet />
